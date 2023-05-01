e08b312eb78c83f773d57c9d86bdc1f343ec3384
Jamie Stirrat <jamiestirrat@hotmail.co.uk>
2023-05-01 17:40:52 +0100

    fix typing bug

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -251,22 +251,16 @@ export const ChatMessage: FC<Props> = memo(
                   rehypePlugins={[rehypeMathjax]}
                   components={{
                     code({ node, inline, className, children, ...props }) {
-                      if (children.length) {
-                        if (children[0] == '▍') {
-                          return (
-                            <span className="animate-pulse cursor-default mt-1">
-                              ▍
-                            </span>
-                          );
-                        }
-
-                        children[0] = (children[0] as string).replace(
-                          '`▍`',
-                          '▍',
-                        );
-                      }
-
                       const match = /language-(\w+)/.exec(className || '');
+
+                      if (children[0] === '▍') {
+                        children =
+                          messageIsStreaming &&
+                          messageIndex ==
+                            (selectedConversation?.messages.length ?? 0) - 1
+                            ? ['▍']
+                            : [];
+                      }
 
                       return !inline ? (
                         <CodeBlock
@@ -305,10 +299,9 @@ export const ChatMessage: FC<Props> = memo(
                   }}
                 >
                   {`${message.content}${
-                    messageIsStreaming &&
                     messageIndex ==
-                      (selectedConversation?.messages.length ?? 0) - 1
-                      ? '`▍`'
+                    (selectedConversation?.messages.length ?? 0) - 1
+                      ? '▍'
                       : ''
                   }`}
                 </MemoizedReactMarkdown>
